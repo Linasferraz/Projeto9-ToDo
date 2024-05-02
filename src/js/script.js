a5dec52986154a6b6d7eb0e4068ed9e87379ab00
Carolina <carolinasferraz80@gmail.com>
2024-05-02 11:02:59 -0300

    Criando função adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -8,5 +8,26 @@ function validaCampo()
 {
     let valida=false;
     if(document.getElementById("task").value =="")
-    
+    valida=
+    return valida;
+}
+
+// Função que adiciona a tarefa
+
+function adicionarTarefa(){
+
+    let linhas =document.getElementById("task");
+
+    if(validaCampo()){
+        alert("Preencha o campo com a tarefa")
+    }else{
+        // adicionando as tarefas
+        tarefas.push(linhas.value);
+        // passando para a proxima linha vazia
+        linhas.value="";
+        // chamando a função que vai mostrar todas as tarefas
+        listarTarefas();
+    }
+    // volte para o input da tarefa
+    document.getElementById("task").focus();
 }
